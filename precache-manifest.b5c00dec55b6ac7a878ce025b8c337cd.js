6db3330aacdc4390392665225b21f811a9f12e07
pride muchirawetu <pridemuchirawetu@prides-MacBook-Pro.local>
2020-05-24 06:27:53 +0200

    Updates

---
+++ precache-manifest.b5c00dec55b6ac7a878ce025b8c337cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5485107ae1444e1629063ba882ba7275",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "faa25aa4cb8cd99ec327",
+    "url": "/monsters-rolodex/static/css/main.0a6c6db2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa25aa4cb8cd99ec327",
+    "url": "/monsters-rolodex/static/js/main.230e7990.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
